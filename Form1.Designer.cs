dd1a0193dbe6cbb0225ec701d70fda4af51a2841
Helios-S <34891974+Helios-S@users.noreply.github.com>
2018-06-09 22:17:16 +0530

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ScreenCaptureDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonCapture = new System.Windows.Forms.Button();
+            this.Capture = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCapture
+            // 
+            this.buttonCapture.Location = new System.Drawing.Point(148, 2);
+            this.buttonCapture.Name = "buttonCapture";
+            this.buttonCapture.Size = new System.Drawing.Size(52, 42);
+            this.buttonCapture.TabIndex = 0;
+            this.buttonCapture.Text = "Capture";
+            this.buttonCapture.UseVisualStyleBackColor = true;
+            this.buttonCapture.Click += new System.EventHandler(this.buttonCapture_Click);
+            // 
+            // Capture
+            // 
+            this.Capture.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.Capture.BalloonTipText = "SC";
+            this.Capture.BalloonTipTitle = "SC";
+            this.Capture.ContextMenuStrip = this.contextMenuStrip1;
+            this.Capture.Icon = ((System.Drawing.Icon)(resources.GetObject("Capture.Icon")));
+            this.Capture.Text = "Capture";
+            this.Capture.Visible = true;
+            this.Capture.Click += new System.EventHandler(this.Capture_Click);
+            this.Capture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Capture_MouseClick);
+            this.Capture.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuItem1.Text = "Close";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(127, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 45);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.MinimumSizeChanged += new System.EventHandler(this.Form1_MinimumSizeChanged);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCapture;
+        private System.Windows.Forms.NotifyIcon Capture;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ScreenCaptureDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCapture_Click(object sender, EventArgs e)
+        {
+           // this.Visible = false;
+            this.Hide();
+            
+            CaptureMyScreen();
+         ////   this.Visible = true;
+            this.Show();
+        }
+
+        private void CaptureMyScreen()
+        {   
+            try
+            {
+                System.Threading.Thread.Sleep(500);
+                //Creating a new Bitmap object
+                Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+                //Creating a Rectangle object which will capture our Current Screen
+                  Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+
+                //Creating a New Graphics Object
+                  Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+            //Copying Image from The Screen
+           
+                  captureGraphics.CopyFromScreen(captureRectangle.Left,captureRectangle.Top,0,0,captureRectangle.Size);
+           
+                //Saving the Image File (I am here Saving it in My E drive).
+                string path = textBox1.Text;
+                string filename=null;
+            if (path.Last().ToString() != @"\")
+                {
+                path = path + @"\";
+            }
+                if (!(textBox2.Text == ""))
+                {
+                    filename = textBox2.Text + ".jpg";
+                }
+                else { filename =  System.Guid.NewGuid().ToString()+".jpg"; }
+                captureBitmap.Save(path+filename,ImageFormat.Jpeg);
+
+                //Displaying the Successfull Result
+
+                //  MessageBox.Show("Screen Captured");
+               
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message+"\n Run as Admin");
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Capture_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if(!checkBox1.Checked)
+            textBox2.Text = "";
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+   Application.Exit();
+        }
+
+        private void Form1_MinimumSizeChanged(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void Capture_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
